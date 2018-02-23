6c15ca8758789b6124e079ada6ce9b205c0c1758
Gustavo Sanchez <gsanchez@musi.com.ar>
2018-02-23 18:00:42 -0300

    -cambio en la conexion a la db

--- src/main/java/DataBase/DBConectionManager.java
+++ src/main/java/DataBase/DBConectionManager.java
@@ -27,7 +27,8 @@ public class DBConectionManager
         {
 //            c = DriverManager.getConnection("jdbc:mysql://localhost/Feed?useSSL=false", DB_USERNAME, DB_PASSWORD);
             c = DriverManager.getConnection("jdbc:mysql://localhost/feed_test?useSSL=false", DB_USERNAME, DB_PASSWORD);
-//            c = DriverManager.getConnection("jdbc:mysql://mysq14505-musiwebapp.jelastic.saveincloud.net/feed", "user", "sWGgjGnHwbI1mORB");
+//            c = DriverManager.getConnection("jdbc:mysql://localhost/feed_test?useSSL=false", DB_USERNAME, DB_PASSWORD);
+
             c.setAutoCommit(false);
         }
         catch (SQLException e)
