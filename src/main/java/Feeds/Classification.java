f16e562584d80d5dd9d4561eae6af49334eff9f0
Gustavo Sanchez <gsanchez@musi.com.ar>
2018-03-13 16:30:15 -0300

    -se implemento el feed classification

--- src/main/java/Feeds/Classification.java
+++ src/main/java/Feeds/Classification.java
@@ -1,4 +1,37 @@
 package Feeds;
 
-public class Classification extends FeedPadre {
+public class Classification extends FeedPadre
+{
+    private String codigoAtributo, codigoCategoria, valorAtributo;
+
+    public Classification()
+    {
+        codigoAtributo = null;
+        codigoCategoria = null;
+        valorAtributo = null;
+    }
+
+    public String getCodigoAtributo() {
+        return codigoAtributo;
+    }
+
+    public void setCodigoAtributo(String codigoAtributo) {
+        this.codigoAtributo = codigoAtributo;
+    }
+
+    public String getCodigoCategoria() {
+        return codigoCategoria;
+    }
+
+    public void setCodigoCategoria(String codigoCategoria) {
+        this.codigoCategoria = codigoCategoria;
+    }
+
+    public String getValorAtributo() {
+        return valorAtributo;
+    }
+
+    public void setValorAtributo(String valorAtributo) {
+        this.valorAtributo = valorAtributo;
+    }
 }
