a6c601b3904d3a9631f39785c34a148f76701091
Gustavo Sanchez <gsanchez@musi.com.ar>
2018-08-14 14:12:15 -0300

    -se agregó a Nahuel a la lista de mails

--- src/main/java/utilidades/Mail.java
+++ src/main/java/utilidades/Mail.java
@@ -17,8 +17,8 @@ public class Mail
     private final String EMAILSENDER = "gsanchez@musi.com.ar";
     private final String PWD = "fff0303456fff";
     private String[] destinatarios = {"gsanchez@musi.com.ar"};
-    private String[] destinatariosCARSA = {"gsanchez@musi.com.ar", "cbaez@musi.com.ar", "jbasombr@musi.com.ar", "marcelo.hassan@grupocarsa.com", "nestor.gatter@grupocarsa.com", "federico.henchoz@grupocarsa.com"};
-    private String[] destinatariosEMSA = {"gsanchez@musi.com.ar", "cbaez@musi.com.ar", "jbasombr@musi.com.ar", "alejandro.brun@emusimundo.com", "amartin@conmega.com.ar", "mbiancul@emusimundo.com","lparera@emusimundo.com","enrique.ferreyra@emusimundo.com"};
+    private String[] destinatariosCARSA = {"gsanchez@musi.com.ar", "cbaez@musi.com.ar", "jbasombr@musi.com.ar", "ngarcia@musi.com.ar", "marcelo.hassan@grupocarsa.com", "nestor.gatter@grupocarsa.com", "federico.henchoz@grupocarsa.com"};
+    private String[] destinatariosEMSA = {"gsanchez@musi.com.ar", "cbaez@musi.com.ar", "jbasombr@musi.com.ar", "ngarcia@musi.com.ar", "alejandro.brun@emusimundo.com", "amartin@conmega.com.ar", "mbiancul@emusimundo.com","lparera@emusimundo.com","enrique.ferreyra@emusimundo.com"};
 //    private String[] destinatariosCARSA = {"gsanchez@musi.com.ar"};
 //    private String[] destinatariosEMSA = {"gsanchez@musi.com.ar"};
 
